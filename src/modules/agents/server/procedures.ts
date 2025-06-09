50bfbf02797773ef519ef2b451a940bc55d58f3d
shoaibakhtar800 <cs08018akp@gmail.com>
2025-06-09 18:40:29 +0530

    minor changes

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -2,24 +2,63 @@ import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schemas";
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
     getMany: protectedProcedure
-        .query(async () => {
-            const data = await db
-                .select()
-                .from(agents);
+        .input(z.object({
+            page: z.number().default(DEFAULT_PAGE),
+            pageSize: z.number().min(MIN_PAGE_SIZE).max(MAX_PAGE_SIZE).default(DEFAULT_PAGE_SIZE),
+            search: z.string().nullish()
+        }))
+        .query(async ({ ctx, input }) => {
+            const { search, page, pageSize } = input;
 
-            return data;
+            const data = await db
+                .select({
+                    meetingCount: sql<number>`5`,
+                    ...getTableColumns(agents),
+                })
+                .from(agents)
+                .where(
+                    and(
+                        eq(agents.userId, ctx.auth.user.id),
+                        search ? ilike(agents.name, `%${search}%`) : undefined
+                    )
+                )
+                .orderBy(desc(agents.createdAt), desc(agents.id))
+                .limit(pageSize)
+                .offset((page - 1) * pageSize);
+
+                const [total] = await db
+                    .select({ count: count() })
+                    .from(agents)
+                    .where(
+                        and(
+                            eq(agents.userId, ctx.auth.user.id),
+                            search ? ilike(agents.name, `%${search}%`) : undefined
+                        )
+                    );
+
+                const totalPages = Math.ceil(total.count / pageSize);
+
+            return {
+                items: data,
+                total: total.count,
+                totalPages
+            };
         }),
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
         .query(async ({ input }) => {
             const [existingAgentById] = await db
-                .select()
+                .select({
+                    meetingCount: sql<number>`5`,
+                    ...getTableColumns(agents)
+                })
                 .from(agents)
                 .where(eq(agents.id, input.id))
 
--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button"
 import { NewAgentDialog } from "./new-agent-dialog";
-import { useState } from "react";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { AgentsSearchFilter } from "./agents-search-filter";
+import { DEFAULT_PAGE } from "@/constants";
 
 export const AgentsListHeader = () => {
+    const [filters, setFilters] = useAgentsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    const isAnyFilterModified = !!filters.search;
+
+    const onClearFilters = () => {
+        setFilters({
+            search: "",
+            page: DEFAULT_PAGE
+        })
+    }
 
     return (
         <>
@@ -19,6 +32,15 @@ export const AgentsListHeader = () => {
                         <PlusIcon />
                         New Agent
                     </Button>
+                </div>
+                <div className="flex items-center gap-x-2 p-1">
+                    <AgentsSearchFilter />
+                    {isAnyFilterModified && (
+                       <Button variant="outline" size="sm" onClick={onClearFilters}>
+                        <XCircleIcon />
+                        Clear
+                       </Button> 
+                    )}
                 </div>
             </div>
         </>
