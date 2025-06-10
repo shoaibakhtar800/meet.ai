d5922041a12a834c3724d657b264f5671f2b1b22
shoaibakhtar800 <cs08018akp@gmail.com>
2025-06-10 10:08:05 +0530

    minor change

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentsRouter } from '@/modules/agents/server/procedures';
 import { createTRPCRouter } from '../init';
+import { meetingsRouter } from '@/modules/meetings/server/procedure';
 
 export const appRouter = createTRPCRouter({
-  agents: agentsRouter
+  agents: agentsRouter,
+  meetings: meetingsRouter
 });
 
 // export type definition of API
