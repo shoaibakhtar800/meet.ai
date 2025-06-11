c75243dbfb262556d2bd3c87ddba003c69140c53
shoaibakhtar800 <cs08018akp@gmail.com>
2025-06-11 10:32:00 +0530

    minor change

--- src/modules/meetings/server/procedure.ts
+++ src/modules/meetings/server/procedure.ts
@@ -10,6 +10,25 @@ import { TRPCError } from "@trpc/server";
 import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
+    remove: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const [removedMeeting] = await db
+                .delete(meetings)
+                .where(
+                    and(
+                        eq(meetings.id, input.id),
+                        eq(meetings.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+
+            if (!removedMeeting) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found" });
+            }
+
+            return removedMeeting;
+        }),
     getMany: protectedProcedure
         .input(z.object({
             page: z.number().default(DEFAULT_PAGE),
@@ -75,9 +94,12 @@ export const meetingsRouter = createTRPCRouter({
         .query(async ({ input, ctx }) => {
             const [existingMeetingById] = await db
                 .select({
-                    ...getTableColumns(meetings)
+                    ...getTableColumns(meetings),
+                    agent: agents,
+                    duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
                 })
                 .from(meetings)
+                .innerJoin(agents, eq(meetings.agentId, agents.id))
                 .where(
                     and(
                         eq(meetings.id, input.id),
